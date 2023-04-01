a6f1f978d91c8f73b54956e6bf5d9828092ae5fa
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-01 16:36:58 -0300

    Banco_Dev- Inicial

---
+++ Banco_Dev/Banco_Dev/Controllers/ClienteController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Banco_Dev;
+
+namespace Banco_Dev.Controllers
+{
+    public class ClienteController : Controller
+    {
+        [HttpPost("Clientes")]
+        public ActionResult PessoaFisica([FromBody] Cliente PessoaFisica)
+        {
+            return Ok("Created");
+        }
+        [HttpPost("Clientes")]
+        public ActionResult PessoaJuridica([FromBody] Cliente PessoaJuridica)
+        {
+            return Ok("Created");
+        }
+        [HttpGet("Clientes")]
+        public ActionResult ObterClientes()
+        {
+            return Ok("Success");
+        }
+        [HttpGet("Clientes/{id}")]
+        public ActionResult ObterClientes(int id)
+        {
+            return Ok("Success");
+        }
+        [HttpPut("Clientes/{id}")]
+        public ActionResult PessoaFisica(int id)
+        {
+            return Ok("Success");
+        }
+        [HttpPut("Clientes/{id}")]
+        public ActionResult PessoaJuridica(int id)
+        {
+            return Ok("Success");
+        }
+        [HttpDelete("Clientes/{id}")]
+        public ActionResult ExcluirCliente(int id)
+        {
+            if (Cliente saldo<=0)
+            {
+                return Ok("conta excluida");
+            } else 
+                {
+                    return BadRequest("Não foi possivel excluir a conta pois o saldo não está zerado");
+                }
+        }
+
+    }
+}
---
+++ Banco_Dev/Banco_Dev/Controllers/TransacaoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Banco_Dev;
+
+namespace Banco_Dev.Controllers
+{
+    public class TransacaoController : Controller
+    {
+
+        [HttpPost] ("Transacao/{idCliente}")
+        
+        public ActionResult PostTransacao(int idCliente)
+        {
+            
+                return Results.Ok(transacao);
+            
+        
+        }
+        [HttpGet]("Transacoes/{idCliente}")
+            public ActionResult ListTransasao(int idCliente)
+        {
+            return Ok(ClienteModel.Transacao);
+        }
+    }
+
+       
+}
